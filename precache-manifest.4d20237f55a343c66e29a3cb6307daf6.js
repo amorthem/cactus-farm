6febc6b1d780fbe2a05009d93e607e5bfa86b727
amorthem <noom-pn@hotmail.com>
2021-06-13 07:00:41 +0700

    Updates

---
+++ precache-manifest.4d20237f55a343c66e29a3cb6307daf6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9063a429fb6dfda8275617c4a0bf4b4",
+    "url": "/cactus-farm/index.html"
+  },
+  {
+    "revision": "4b67ed44a6521776ccf0",
+    "url": "/cactus-farm/static/css/main.e5619b06.chunk.css"
+  },
+  {
+    "revision": "6645e66d34afa0cfbccf",
+    "url": "/cactus-farm/static/js/0.7d971863.chunk.js"
+  },
+  {
+    "revision": "3465aa035095b9f41677",
+    "url": "/cactus-farm/static/js/10.a0396058.chunk.js"
+  },
+  {
+    "revision": "3c7d116c74e594cc2df7",
+    "url": "/cactus-farm/static/js/3.6397cc5a.chunk.js"
+  },
+  {
+    "revision": "10eeaea8d5be9a1b995611f174d5993e",
+    "url": "/cactus-farm/static/js/3.6397cc5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a0edf44843df27444d",
+    "url": "/cactus-farm/static/js/4.befb7238.chunk.js"
+  },
+  {
+    "revision": "2b24a9516601410cb125",
+    "url": "/cactus-farm/static/js/5.f75a5dd4.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/cactus-farm/static/js/5.f75a5dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9846c21f14a5a1290371",
+    "url": "/cactus-farm/static/js/6.f8664cc3.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/cactus-farm/static/js/6.f8664cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d436468777e81630a8a",
+    "url": "/cactus-farm/static/js/7.e60cac31.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/cactus-farm/static/js/7.e60cac31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a1317d1f7dec34a3bd",
+    "url": "/cactus-farm/static/js/8.0f7f91ee.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/cactus-farm/static/js/8.0f7f91ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0ae72d1c96f368f052",
+    "url": "/cactus-farm/static/js/9.e676443b.chunk.js"
+  },
+  {
+    "revision": "4b67ed44a6521776ccf0",
+    "url": "/cactus-farm/static/js/main.ef4187fb.chunk.js"
+  },
+  {
+    "revision": "9bbe56e6e5b21a7cd0a8",
+    "url": "/cactus-farm/static/js/runtime-main.b1e6dabd.js"
+  },
+  {
+    "revision": "a3a11c7aa249fe7e37db17dbf1557ef1",
+    "url": "/cactus-farm/static/media/Cup.a3a11c7a.svg"
+  },
+  {
+    "revision": "5846ff06a6e79cf621c4180f047bb14f",
+    "url": "/cactus-farm/static/media/coin98.5846ff06.png"
+  },
+  {
+    "revision": "6062483fe03da980771098fc5d9e349d",
+    "url": "/cactus-farm/static/media/icon-view.6062483f.svg"
+  },
+  {
+    "revision": "721de13835e0f85cc114bb251afc14fd",
+    "url": "/cactus-farm/static/media/logo.721de138.721de138.png"
+  },
+  {
+    "revision": "8f5d93d024c3c03551d8f70bf2f539fd",
+    "url": "/cactus-farm/static/media/logo.8f5d93d0.svg"
+  },
+  {
+    "revision": "156664c9217e8a4a677fcc28f55f43ca",
+    "url": "/cactus-farm/static/media/menu.156664c9.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/cactus-farm/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "/cactus-farm/static/media/trustwallet.edcc1ab5.png"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/cactus-farm/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
